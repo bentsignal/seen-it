127d1debe6eee426cdb7613d2f391f04950f4a41
Shawn Rodgers <me@bentsignal.com>
2026-01-05 23:53:20 -0500

    fix firefox manifest, add license

--- vite.config.firefox.ts
+++ vite.config.firefox.ts
@@ -1,9 +1,9 @@
-import { resolve } from 'path';
-import { mergeConfig, defineConfig } from 'vite';
-import { crx, ManifestV3Export } from '@crxjs/vite-plugin';
-import baseConfig, { baseManifest, baseBuildOptions } from './vite.config.base';
+import { resolve } from "path";
+import { mergeConfig, defineConfig } from "vite";
+import { crx, ManifestV3Export } from "@crxjs/vite-plugin";
+import baseConfig, { baseManifest, baseBuildOptions } from "./vite.config.base";
 
-const outDir = resolve(__dirname, 'dist/firefox');
+const outDir = resolve(__dirname, "dist/firefox");
 
 export default mergeConfig(
   baseConfig,
@@ -13,16 +13,20 @@ export default mergeConfig(
         manifest: {
           ...baseManifest,
           background: {
-            scripts: ['src/background/index.ts'],
+            scripts: ["src/background/index.ts"],
           },
           browser_specific_settings: {
             gecko: {
-              id: '{cba53012-7a0d-4559-9bee-c5df54aa0658}',
-              strict_min_version: '109.0',
+              id: "{cba53012-7a0d-4559-9bee-c5df54aa0658}",
+              strict_min_version: "109.0",
+              data_collection_permissions: {
+                required: ["none"],
+                optional: [],
+              },
             },
           },
         } as ManifestV3Export,
-        browser: 'firefox',
+        browser: "firefox",
         contentScripts: {
           injectCss: true,
         },
